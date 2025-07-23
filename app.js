d287a4630a1ba5959b3ab8a25abbcf877a9e131e
robert <robertrenbysanjuan@gmail.com>
2025-07-23 19:06:07 +0800

    Set up basic user authentication routes, DB connection, and global middleware

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-require('dotenv').config
+require('dotenv').config()
 require('express-async-errors')
 
 const express = require('express');
 const app = express();
+const userRouter = require('./router/userRouter')
+const connectDB = require('./db/connect')
 const cors = require('cors')
 const { errorHandler } = require('./util')
 
 app.use(express.json())
 
-
+app.use('/api/v1', userRouter)
 
 cors()
 
 app.use(errorHandler)
 
 const port = process.env.PORT || 3000
-const start = () => {
+const start = async () => {
     try{
+        await connectDB(process.env.MONGO_URL)
         app.listen(port)
         console.log('server is listening in port:', port);
     }catch(err){
